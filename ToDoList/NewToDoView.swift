5339658f3ebfb8d8db813efaf4943c1dc1075a56
Marnie Reid <marnie@kodewithklossy.com>
2024-04-17 11:57:41 -0400

    Initial Commit

---
+++ ToDoList/NewToDoView.swift
@@ -0,0 +1,18 @@
+//
+//  NewToDoView.swift
+//  ToDoList
+//
+//  Created by scholar on 4/17/24.
+//
+
+import SwiftUI
+
+struct NewToDoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewToDoView()
+}
