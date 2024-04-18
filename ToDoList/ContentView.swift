9491beb23cfe77ee3ecbbe65729db345e1561e4a
Marnie Reid <marnie@kodewithklossy.com>
2024-04-18 11:57:54 -0400

    Complete Iteration 0

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,14 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var showNewTask = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            HStack {
+                Text("ToDo List")
+                    .font(.system(size: 40))
+                    .fontWeight(.black)
+                
+                Spacer()
+                
+                Button {
+                    withAnimation {
+                        self.showNewTask = true
+                    }
+                    
+                } label: {
+                    Text("+")
+                        .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                        .bold()
+                }
+            }
+            .padding()
+            
+            Spacer()
+            
         }
-        .padding()
+        if showNewTask {
+            NewToDoView()
+        }
     }
 }
 
--- ToDoList/NewToDoView.swift
+++ ToDoList/NewToDoView.swift
@@ -9,7 +9,28 @@ import SwiftUI
 
 struct NewToDoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Task title:")
+                .font(.title2)
+                .fontWeight(.bold)
+            
+            TextField("Enter the task description...", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                .padding()
+                .background(Color(.systemGroupedBackground))
+                .cornerRadius(15)
+                .padding()
+            
+            Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) {
+                Text("Is it important?")
+            }
+            
+            Button {
+                
+            } label: {
+                Text("Save")
+            }
+        }
+        .padding()
     }
 }
 
